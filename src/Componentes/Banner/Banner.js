a1cd3153e44ec188217f2c89cb0187c687baab7a
Wandson Gomes <wandsongomes0607@gmail.com>
2026-03-22 10:15:52 -0300

    banner criado

---
+++ src/Componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import "./Banner.css"
+function Banner() {
+    // JSX
+    return (
+                //classname é para definr uma classe no jsx
+        <header className="banner">
+            <img src="/img/banner.png"/>
+        </header>
+        
+    )
+}
+export default Banner
